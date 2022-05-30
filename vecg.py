8aab80307e10828392935db259cdab7abdb97fd6
Tulika Arun <64515225+tuls-png@users.noreply.github.com>
2022-05-30 13:09:42 +0530

    Update vecg.py

--- vecg.py
+++ vecg.py
@@ -9,7 +9,7 @@ from keras.models import Model
 import cv2 as cv
 import array as arr
 
-df = pd.read_csv('combined-ptb.csv', header=None)
+df = pd.read_csv('finalcombined.csv', header=None)
 df.head()
 
 # Now we will separate the data and labels so that it will be easy for us
@@ -18,11 +18,12 @@ labels = df.iloc[:, -1].values
 print("data",data)
 print("labels",labels)
 
-train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=0.2, random_state=21)
-
+train_data, test_data, train_labels, test_labels = train_test_split(data, labels, test_size=0.2, random_state=42)
+print("trailabeb",train_labels)
 min = tf.reduce_min(train_data)
 max = tf.reduce_max(train_data)
-
+print("min",min)
+print("max",max)
 # Now we will use the formula (data - min)/(max - min)
 train_data = (train_data - min) / (max - min)
 test_data = (test_data - min) / (max - min)
@@ -32,6 +33,9 @@ train_data = tf.cast(train_data, dtype=tf.float32)
 test_data = tf.cast(test_data, dtype=tf.float32)
 print("train_data",train_data)
 print("test_data",test_data)
+print("?????????????????????????????????????????????????")
+print(train_labels)
+print(test_labels)
 # The labels are either 0 or 1, so I will convert them into boolean(true or false)
 train_labels = train_labels.astype(bool)
 test_labels = test_labels.astype(bool)
@@ -39,6 +43,10 @@ print("train_labels",train_labels)
 print("test_labels",test_labels)
 # Now let's separate the data for normal ECG from that of abnormal ones
 # Normal ECG data
+print("#################################")
+print(train_labels)
+print(~train_labels)
+print("#################################")
 n_train_data = train_data[train_labels]
 n_test_data = test_data[test_labels]
 print("n_train_data",n_train_data)
@@ -53,14 +61,15 @@ print("an_test_data",an_test_data)
 print("Abnormal Train Data\n",an_train_data)
 print("Abnormal Test Data\n",an_test_data)
 
+print(an_test_data[0][-1])
 # Lets plot a normal ECG
-plt.plot(np.arange(187), n_train_data[1])
+plt.plot(np.arange(187), n_train_data[0])
 plt.grid()
 plt.title('Normal ECG')
 plt.show()
 
 # Lets plot one from abnormal ECG
-plt.plot(np.arange(187), an_train_data[1])
+plt.plot(np.arange(187), an_train_data[0])
 plt.grid()
 plt.title('Abnormal ECG')
 plt.show()
@@ -91,9 +100,10 @@ class detector(Model):
 # Let's compile and train the model!!
 autoencoder = detector()
 autoencoder.compile(optimizer='adam', loss='mae')
-autoencoder.fit(train_data, train_data, epochs=20, batch_size=512, validation_data=(test_data, test_data))
-
-
+history = autoencoder.fit(n_train_data, n_train_data, epochs=100, batch_size=512, validation_data=(test_data, test_data))
+plt.plot(history.history["loss"], label="Training Loss")
+plt.plot(history.history["val_loss"], label="Validation Loss")
+plt.show()
 # Now let's define a function in order to plot the original ECG and reconstructed ones and also show the error
 def plot(data, n):
     enc_img = autoencoder.encoder(data)
@@ -109,21 +119,27 @@ plot(n_test_data, 0)
 print("n_test_data, 0")
 
 
-reconstructed = autoencoder(train_data)
-train_loss = losses.mae(reconstructed, train_data)
+reconstructed = autoencoder.predict(n_train_data)
+train_loss = tf.keras.losses.mae(reconstructed, n_train_data)
+
 t = np.mean(train_loss) + np.std(train_loss)
+print("Threshold", t)
 
 
-def prediction(model, data, threshold):
-    rec = model(data)
-    loss = losses.mae(rec, data)
-    return tf.math.less(loss, threshold)
+reconstructed = autoencoder.predict(an_test_data)
+test_loss = tf.keras.losses.mae(reconstructed, an_test_data)
+
+plt.hist(train_loss[None,:], bins=50, label='Train', alpha=0.7, edgecolor='red')
+plt.hist(test_loss[None,:], bins=50, label='Test', alpha=0.7, edgecolor='yellow')
+plt.xlabel("Loss")
+plt.ylabel("Number of examples")
+plt.legend()
+plt.show()
 
 
-print(t)
+print(train_loss)
+print(test_loss)
 
-pred = prediction(autoencoder, test_data, t)
-print(pred)
 
 plot(an_test_data, 0)
 print("an_test_data, 0")
@@ -131,7 +147,10 @@ print("an_test_data, 0")
 plot(an_test_data, 1)
 print("an_test_data, 1")
 plot(an_test_data, 2)
-print("an_test_data, 2")
+
+
+
+
 
 #Now we display our activations
 number = int(input("Enter the amount of data you would like to see:"))
@@ -227,9 +246,3 @@ plt.xticks(np.arange(1, len(antest1)+1))
 
 plt.legend()
 plt.show()
-
-
-
-
-
-
